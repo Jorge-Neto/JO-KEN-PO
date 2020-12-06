099f4a65659ae96d8f083609fba14c9e66c57967
Jorge-Neto <53627696+Jorge-Neto@users.noreply.github.com>
2020-12-06 20:04:06 -0300

    Adição das imagens e finalização do código

--- js/programa.js
+++ js/programa.js
@@ -28,14 +28,14 @@ function calcularEscolha(jogador, computador) {
         return 2;
     } else if (jogador == 1 && computador == 3) {
         return 1;
-    } 
+    }
     else if (jogador == 2 && computador == 1) {
         return 1;
     } else if (jogador == 2 && computador == 2) {
         return 0;
     } else if (jogador == 2 && computador == 3) {
         return 2;
-    } 
+    }
     else if (jogador == 3 && computador == 1) {
         return 2;
     } else if (jogador == 3 && computador == 2) {
@@ -55,13 +55,23 @@ function somarPontoComputador() {
     document.getElementById('computador-pontos').innerHTML = computadorPontos;
 }
 
+function selecionar(tipo, escolha) {
+    document.getElementById(tipo + '-escolha' + escolha).classList.add('selecionado');
+}
+
+function deselecionar(tipo, escolha) {
+    document.getElementById(tipo + '-escolha' + escolha).classList.remove('selecionado');
+}
+
 //Escolhe a jogada do usuário
 function jogar(escolha) {
     jogadorEscolha = escolha;
+    selecionar('jogador', jogadorEscolha);
 
     //Sortear a jogada do COM
-    computadorEscolha = sortear(1,3);
-    
+    computadorEscolha = sortear(1, 3);
+    selecionar('computador', computadorEscolha);
+
     //Calcular quem ganhou
     var ganhador = calcularEscolha(jogadorEscolha, computadorEscolha);
     if (ganhador == 0) {
@@ -73,20 +83,23 @@ function jogar(escolha) {
         mensagem('Computador levou essa!');
         somarPontoComputador();
     }
-    //Somar pontos
-    //Exibir mão e pontuação
+
+    setTimeout(
+        function () {
+            deselecionar('jogador', jogadorEscolha);
+            deselecionar('computador', computadorEscolha);
+            
+            mensagem(jogadorNome + ' escolha uma opção');
+        }, 3000
+    );
 }
 
-document.getElementById('jogador-escolha1').onclick = function() {
-    jogar(1); };
-document.getElementById('jogador-escolha2').onclick = function() {
-    jogar(2);};
-document.getElementById('jogador-escolha3').onclick = function() {
-    jogar(3); };
+document.getElementById('jogador-escolha1').onclick = function () {jogar(1)};
+document.getElementById('jogador-escolha2').onclick = function () {jogar(2)};
+document.getElementById('jogador-escolha3').onclick = function () {jogar(3)};
 
 jogadorNome = prompt('Qual é o seu nome?');
 mensagem('Bem-vindo ' + jogadorNome + ', está preparado? Escolha uma opção acima');
 definirNomeJogador(jogadorNome);
 
-//DOM
-//Document Object Model
+//DOM = Document Object Model
